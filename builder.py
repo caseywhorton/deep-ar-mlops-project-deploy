35c311574ffa6560351fe5ed0dfc6154a6dd78b2
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2023-12-10 16:04:45 -0500

    Update builder.py

--- builder.py
+++ builder.py
@@ -6,21 +6,14 @@ import yaml
 import boto3
 from botocore.exceptions import ClientError
 
-# Set up logging
 logger = logging.getLogger(__name__)
 log_format = "%(levelname)s: [%(filename)s:%(lineno)s] %(message)s"
 logging.basicConfig(format=log_format)
 
-# AWS client setup
 sm_client = boto3.client("sagemaker")
 
-
-
-
-# Fetch latest approved model package for a model package group
 def get_approved_package(model_package_group_name):
     try:
-        # Fetch approved packages
         response = sm_client.list_model_packages(
             ModelPackageGroupName=model_package_group_name,
             ModelApprovalStatus="Approved",
@@ -29,7 +22,6 @@ def get_approved_package(model_package_group_name):
         )
         approved_packages = response["ModelPackageSummaryList"]
 
-        # Fetch more packages if none returned with continuation token
         while len(approved_packages) == 0 and "NextToken" in response:
             response = sm_client.list_model_packages(
                 ModelPackageGroupName=model_package_group_name,
@@ -40,13 +32,11 @@ def get_approved_package(model_package_group_name):
             )
             approved_packages.extend(response["ModelPackageSummaryList"])
 
-        # Return error if no packages found
         if len(approved_packages) == 0:
             error_message = f"No approved ModelPackage found for ModelPackageGroup: {model_package_group_name}"
             logger.error(error_message)
             raise Exception(error_message)
 
-        # Return model package ARN
         model_package_arn = approved_packages[0]["ModelPackageArn"]
         logger.info(f"Identified the latest approved model package: {model_package_arn}")
         return model_package_arn
@@ -55,37 +45,10 @@ def get_approved_package(model_package_group_name):
         logger.error(error_message)
         raise Exception(error_message)
 
-
-# Custom constructor and representer for !Ref and !Sub tags
-def ref_constructor(loader, node):
-    value = loader.construct_scalar(node)
-    return {"Ref": value}
-
-
-def sub_constructor(loader, node):
-    value = loader.construct_scalar(node)
-    return {"Fn::Sub": value}
-
-
-def sub_ref_representer(dumper, data):
-    return dumper.represent_scalar("!Sub", data["Fn::Sub"])
-
-
-def ref_ref_representer(dumper, data):
-    return dumper.represent_scalar("!Ref", data["Ref"])
-
-
-# Add constructors and representers to YAML loader and dumper
-yaml.SafeLoader.add_constructor("!Ref", ref_constructor)
-yaml.SafeLoader.add_constructor("!Sub", sub_constructor)
-yaml.SafeDumper.add_representer(dict, sub_ref_representer)
-yaml.SafeDumper.add_representer(dict, ref_ref_representer)
-
 def add_model_package_name_to_yaml(template_path, model_package_arn):
     with open(template_path, "r") as file:
         template = yaml.safe_load(file)
 
-    # Add ModelPackageName to the SageMakerModel Properties
     template['Resources']['SageMakerModel']['Properties']['Containers'] = [
         {'ModelPackageName': model_package_arn}
     ]
@@ -93,12 +56,10 @@ def add_model_package_name_to_yaml(template_path, model_package_arn):
     with open(template_path, "w") as file:
         yaml.dump(template, file, default_flow_style=False)
 
-    
-# Add environment variables to a SAM template
 def add_environment_variables(template_path, function_name, variables):
     with open(template_path, "r") as file:
         template = yaml.safe_load(file)
-    logger.info(f"variables: {variables}")
+
     if "Resources" in template and function_name in template["Resources"]:
         function = template["Resources"][function_name]
         if "Properties" in function and "Environment" in function["Properties"]:
@@ -110,10 +71,7 @@ def add_environment_variables(template_path, function_name, variables):
     with open(template_path, "w") as file:
         yaml.dump(template, file, default_flow_style=False)
 
-
-# Main execution
 if __name__ == "__main__":
-    # Argument parser setup
     parser = argparse.ArgumentParser(description="Add environment variables to the SAM template")
     parser.add_argument("--template-path", type=str, default="template.yml", help="Path to the SAM template file")
     parser.add_argument("--function-name", type=str, help="Name of the Lambda function")
@@ -126,17 +84,12 @@ if __name__ == "__main__":
     parser.add_argument("--s3-bucket", type=str, required=True)
     args = parser.parse_args()
 
-    # Set log level
     logging.getLogger().setLevel(args.log_level)
 
-    # Fetch model package ARN
     logger.info("Getting model package ARN.")
     model_package_arn = get_approved_package(args.model_package_group_name)
-
-    # Add the model package arn to the resource section
     add_model_package_name_to_yaml(args.template_path, model_package_arn)
 
-    # Create dictionary of environment variables
     logger.info("Creating a dictionary of environment variables.")
     env_dict = {
         "MODEL_PACKAGE_ARN": model_package_arn,
@@ -146,9 +99,6 @@ if __name__ == "__main__":
         "SAGEMAKER_PROJECT_NAME": args.sagemaker_project_name,
     }
 
-    
-
-    # Add environment variables to file
     logger.info(f"Adding environment variables to file: {args.template_path}")
     add_environment_variables(args.template_path, args.function_name, env_dict)
     
