7218ed6141de905e9feadabd00079e807ecef177
Casey Whorton <27929027+caseywhorton@users.noreply.github.com>
2024-05-13 07:21:28 -0400

    Update params.py

--- params.py
+++ params.py
@@ -1,6 +1,7 @@
 # parameters for runtime
 
-FEATURES = ["properties.relativeHumidity.value", "properties.temperature.value"]
+WEATHER_FEATURES = ['temperature_value', 'relativehumidity_value']
+ELECTRICITY_FEATURES = ['value_demand']
 S3_SERVING_PREFIX_URI = "s3://cw-weather-data-deployment/serving/"
 S3_SERVING_INPUT_URI = "s3://cw-weather-data-deployment/serving/serving.json"
 S3_FORECAST_PREFIX_URI = "s3://cw-weather-data-deployment/forecasts/"
